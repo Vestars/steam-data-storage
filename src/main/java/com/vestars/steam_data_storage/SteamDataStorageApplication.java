2721ad503b6bf96581c9ee1dc805623a3ee9712d
Dmytro Voronin <vestars093@gmail.com>
2025-04-23 06:21:51 +0300

    Initial commit

---
+++ src/main/java/com/vestars/steam_data_storage/SteamDataStorageApplication.java
@@ -0,0 +1,13 @@
+package com.vestars.steam_data_storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteamDataStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteamDataStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vestars/steam_data_storage/SteamDataStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vestars.steam_data_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteamDataStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
