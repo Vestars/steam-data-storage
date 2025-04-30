9b39dc4e36ecef1f25eeb278cebff6ce7cab5ace
Dmytro Voronin <vestars093@gmail.com>
2025-04-30 18:16:25 +0300

    fix

--- src/main/java/com/vestars/steam_data_storage/listener/GameMessageListener.java
+++ src/main/java/com/vestars/steam_data_storage/listener/GameMessageListener.java
@@ -5,17 +5,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vestars.steam_data_storage.entity.Game;
 import com.vestars.steam_data_storage.service.GameService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GameMessageListener {
 
-    private final GameService gameService;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    public GameMessageListener(GameService gameService) {
-        this.gameService = gameService;
-    }
+    @Autowired
+    private GameService gameService;
+
 
     @RabbitListener(queues = "steam-game-queue")
     public void receiveMessage(String message) throws JsonProcessingException {
